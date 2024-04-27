f8cc004ff3a33a2b3747f85f625cbb389affc44e
MARIEM-AMZIL <147107786+MAR-AM@users.noreply.github.com>
2024-04-27 15:02:32 +0100

    Function Evaluation Ensemble.js

---
+++ Function Evaluation Ensemble.js
@@ -0,0 +1,13 @@
+let first = (x)=>{return x*5}
+    function fct1(x){
+        console.log(first())
+    }
+    console.log(fct1())
+    function fct2(x,y){
+        return x+y
+    }
+
+    function fct0(){
+        var nbr = parseFloat(prompt("saisir un nombre  : "))
+        return nbr+2
+    }
